024efef73275270ecb455dc6c3ffee3c3451ec33
unknown <User@User-PC.(none)>
2016-02-23 01:24:55 -0500

    guias a favor

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,33 @@
 'use strict';
 
 angular.module('portalWebApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function($scope, Principal) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
+
+            $scope.roundProgressData = {
+                label: 0,
+                percentage: 0
+            };
+
+            // Here I synchronize the value of label and percentage in order to have a nice demo
+            $scope.$watch('roundProgressData', function(newValue, oldValue) {
+                newValue.percentage = newValue.label / 100;
+            }, true);
+
+            $scope.guiasBovinos = function() {
+                $scope.roundProgressData = {
+                    label: 9,
+                    percentage: 0.9
+                };
+            };
+
+            $scope.guiasPorcinos = function() {
+                $scope.roundProgressData = {
+                    label: 3,
+                    percentage: 0.3
+                };
+            };
         });
     });
