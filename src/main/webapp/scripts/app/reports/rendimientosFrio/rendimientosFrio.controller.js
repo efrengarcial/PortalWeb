1e3b2b1f407b3b0de9eba3f0626d402c07d6909a
Dario Garcia <dariodeath@gmail.com>
2016-04-13 23:05:02 -0500

    reporte inventario

--- src/main/webapp/scripts/app/reports/rendimientosFrio/rendimientosFrio.controller.js
+++ src/main/webapp/scripts/app/reports/rendimientosFrio/rendimientosFrio.controller.js
@@ -8,58 +8,128 @@
  * Controller of the portalWebApp
  */
 angular.module('portalWebApp')
-    .controller('ReporteRendimientosFrioController', ['$scope', '$log', '$filter', 'moment', 'Constants', function($scope, $log, $filter, moment, Constants) {
+    .controller('ReporteRendimientosFrioController', ['$scope', '$log', '$filter', 'moment', 'ClientService', '$sce', 'Principal', 'Constants', function($scope, $log, $filter, moment, ClientService, $sce, Principal, Constants) {
+        Principal.identity().then(function(account) {
+            $scope.account = account;
+            $scope.isAuthenticated = Principal.isAuthenticated;
 
+            $scope.toggleMin = function() {
+                $scope.minDate = moment(Constants.minDate).format(Constants.formatDate);
+            };
 
-        $scope.toggleMin = function() {
-            $scope.minDate = moment(Constants.minDate).format(Constants.formatDate);
-        };
+            $scope.toggleMin();
 
-        $scope.toggleMin();
+            $scope.rendimientosFrio = {
+                StartDate: new Date().getTime(),
+                EndDate: new Date().getTime()
+            };
 
-        $scope.rendimientosFrio = {
-            StartDate:  new Date().getTime(),
-            EndDate:  new Date().getTime()
-        };        
+            $scope.dateOptions = {
+                formatYear: 'yyyy',
+                formatMonth: 'MM',
+                formatDay: 'dd',
+                startingDay: 1
+            };
 
-        $scope.dateOptions = {
-            formatYear: 'yyyy',
-            formatMonth: 'MM',
-            formatDay: 'dd',
-            startingDay: 1
-        };
+            $scope.format = Constants.datepickerFormatDate;
 
-        $scope.format = Constants.datepickerFormatDate;
-        //Auxiliars functions
-        $scope.open = function($event, fecha) {
-            $event.preventDefault();
-            $event.stopPropagation();
-            if ('openedStartDate' === fecha) {
-                $scope.openedStartDate = true;
-            } else {
-                $scope.openedEndDate = true;
+            //Auxiliars functions
+            $scope.open = function($event, fecha) {
+                $event.preventDefault();
+                $event.stopPropagation();
+                if ('openedStartDate' === fecha) {
+                    $scope.openedStartDate = true;
+                } else {
+                    $scope.openedEndDate = true;
+                }
+            };
+
+            function setDataFormInventario() {
+                $scope.rendimientosFrio = {
+                    Marca: "0000",
+                    Productos: [],
+                    TipoProducto: null,
+                    TiposProducto: [],
+                    StartDate: new Date().getTime(),
+                    EndDate: new Date().getTime()                    
+                };
             }
-        };
 
-        $scope.clearForm = function() {
-            $log.debug("clearForm");
-            $scope.rendimientosFrio.StartDate = new Date().getTime();
-            $scope.rendimientosFrio.EndDate = new Date().getTime();
+            function getProductos() {
+                if ($scope.account) {
+                    if ($scope.account.client) {
+                        if ($scope.account.client.productos) {
+                            $scope.rendimientosFrio.Productos = $scope.account.client.productos;
 
-            // Resets the form validation state.
-            $scope.rendimientosFrioForm.$setPristine();
-        };
-
-        $scope.interacted = function(field) {
-            return $scope.submitted || field.$dirty;
-        };
-
-        $scope.submitForm = function(isValid) {
-            if (isValid) {
-                var startDate =  $scope.rendimientosFrio.StartDate;
-                var endDate =  $scope.rendimientosFrio.EndDate;
-                $log.debug("startDate", startDate, "endDate", endDate);
+                            for (var producto in $scope.rendimientosFrio.Productos) {
+                                var tipoProducto = $scope.rendimientosFrio.Productos[producto].tipoProducto;
+                                if (tipoProducto == Constants.B) {
+                                    tipoProducto = Constants.BOVINOS;
+                                    $scope.rendimientosFrio.TiposProducto.push(tipoProducto);
+                                }
+                                if (tipoProducto == Constants.P) {
+                                    tipoProducto = Constants.PORCINOS;
+                                    $scope.rendimientosFrio.TiposProducto.push(tipoProducto);
+                                }
+                            }
+                        }
+                    }
+                }
             }
-        };
+            setDataFormInventario();
+            getProductos();
 
+            function getMarcaProducto(productos, _tipoProducto) {
+                var marca = null;
+                for (var producto in productos) {
+                    var tipoProducto = productos[producto].tipoProducto;
+                    if (_tipoProducto == tipoProducto) {
+                        marca = productos[producto].marca;
+                    }
+                }
+                return marca;
+            }
+
+            $scope.clearForm = function() {
+                $log.debug("clearForm");
+                $scope.rendimientosFrio.StartDate = new Date().getTime();
+                $scope.rendimientosFrio.EndDate = new Date().getTime();
+
+                // Resets the form validation state.
+                $scope.rendimientosFrioForm.$setPristine();
+            };
+
+            $scope.interacted = function(field) {
+                return $scope.submitted || field.$dirty;
+            };
+
+            $scope.submitForm = function(isValid) {
+                if (isValid) {
+                    var startDate = $scope.rendimientosFrio.StartDate;
+                    var endDate = $scope.rendimientosFrio.EndDate;
+                    var tipoProducto = $scope.rendimientosFrio.Productos[1].tipoProducto;
+                    var marca = $scope.rendimientosFrio.Productos[1].marca;
+
+                    $scope.content = "";
+                    ClientService.getReportRendimientoFrio(tipoProducto, marca, startDate, endDate).then(function(blob) {
+                        /*var file = new Blob([blob], {
+                            type: 'application/pdf'
+                        });*/
+                        var fileURL = (window.URL || window.webkitURL).createObjectURL(blob);
+                        //var fileURL = window.URL.createObjectURL(file);
+                        $scope.content = $sce.trustAsResourceUrl(fileURL);
+                        $scope.showPdf = true;
+
+                        //var fileName = "test.pdf";
+                        //var a = document.createElement("a");
+                        //document.body.appendChild(a);
+                        //a.style = "display: none";
+                        //a.href = fileURL;
+                        //a.download = fileName;
+                        //a.click();
+
+                    });
+                }
+            };
+        });
     }]);
--- src/main/webapp/scripts/components/client/client.service.js
+++ src/main/webapp/scripts/components/client/client.service.js
@@ -7,7 +7,27 @@ angular.module('portalWebApp')
                 return APIService.one('client', identificationNumber).get();
             },
             getReportInventarioFrio: function(tipoProducto, marca) {
-                return APIService.one('client',marca).one('inventariofrio',tipoProducto).withHttpConfig({
+                return APIService.one('client', marca).one('inventariofrio', tipoProducto).withHttpConfig({
+                        responseType: 'blob'
+                    })
+                    .get();
+            },
+            getReportInventarioMarca: function(tipoProducto, marca) {
+                /**
+                 * GET  /client/{marca}/inventariomarca/{tipoproducto} -> get report inventario marca detallado.
+                 */
+                return APIService.one('client', marca).one('inventariomarca', tipoProducto).withHttpConfig({
+                        responseType: 'blob'
+                    })
+                    .get();
+            },
+            getReportRendimientoFrio: function(tipoProducto, marca, fromDate, toDate) {
+                console.log("fromDate", fromDate, "toDate", toDate, "tipoProducto", tipoProducto, "marca", marca);
+                /**
+                 * GET  /client/{marca}/rendimientofrio/{tipoProducto}/daterange/{fromDate}/{toDate} ->get report rendimiento frio.
+                 */
+    //localhost:3000/api/client/852/rendimientofrio/P/daterange/05-04-2016/11-04-2016?cacheBuster=1460433402644
+                return APIService.one('client', marca).one('rendimientofrio', tipoProducto).one('daterange', '4-10-2016').one('', '4-11-2016').withHttpConfig({
                         responseType: 'blob'
                     })
                     .get();
